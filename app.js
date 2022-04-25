8e8f1f1256a699a47aa428456d7e6d108c8f8a8f
Avus <47610438+Avu0s@users.noreply.github.com>
2022-04-25 19:52:10 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+const countdown = ()=>{
+    const july = 'July 01, 2022 00:00:00'
+    const julyDate = new Date(july).getTime()
+    const now = new Date().getTime()
+    const diff = julyDate - now
+    
+    const sec = 1000
+    const min = sec * 60
+    const hr = min * 60
+    const day = hr * 24
+
+    const textDay = Math.floor(diff / day)
+    const textHr = Math.floor((diff % day) / hr)
+    const textMin = Math.floor((diff % hr) /min)
+    const textSec = Math.floor((diff % min) / sec)
+
+    const days = document.querySelector('#days').textContent = textDay
+    const hours = document.querySelector('#hours').textContent = textHr
+    const mins = document.querySelector('#mins').textContent = textMin
+    const secs = document.querySelector('#secs').textContent = textSec
+} 
+
+setInterval(countdown,1000)
+
+
